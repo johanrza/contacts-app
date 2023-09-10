602953f4c314eec02915cd533cb2735be8d538d0
Johan R <johanreza58@gmail.com>
2023-09-10 14:17:54 +0700

    Add Delete Contact Feature

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ContactItem from './ContactItem';
 
-function ContactList({ contacts }) {
+function ContactList({ contacts, onDelete }) {
   return (
     <div className="contact-list">
       {/* <ContactItem imageUrl={imageUrl} name={name} tag={tag} /> */}
 
       {/* use loop */}
-      {
-        contacts.map((contact) => (
-          <ContactItem key={contact.id} {...contact} />
-        ))
-      }
+      {contacts.map((contact) => (
+        <ContactItem 
+          key={contact.id} 
+          id={contact.id} 
+          onDelete={onDelete} 
+          {...contact} 
+        />
+      ))}
     </div>
   );
 }
