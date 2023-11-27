e54e2343eef318fc3e942d0e24c073777d183fa3
Johan R <johanreza58@gmail.com>
2023-11-27 09:44:54 +0700

    add contact feature

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactList from './ContactList';
 import { getData } from '../utils/data';
+import ContactInput from './ContactInput';
 
 class ContactApp extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class ContactApp extends React.Component {
     };
 
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onAddContactHandler = this.onAddContactHandler.bind(this);
   }
 
   onDeleteHandler(id) {
@@ -17,11 +19,31 @@ class ContactApp extends React.Component {
     this.setState({ contacts });
   }
 
+  // membawa objek state dari ContactInput di parameter
+  onAddContactHandler({ name, tag }) {
+    this.setState((prevState) => {
+      return {
+        contacts: [
+          ...prevState.contacts, // data lama
+          {
+            id: +new Date(),
+            name,
+            tag,
+            imageUrl: '/images/default.jpg', // diberi scr manual
+          },
+        ],
+      };
+    });
+  }
+
   render() {
     return (
       // punya child
       <div className="contact-app">
-        <h1>Daftar Kontak</h1>
+        <h1>Aplikasi Kontak</h1>
+        <h2>Tambah Kontak</h2>
+        <ContactInput addContact={this.onAddContactHandler} />
+        <h2>Daftar Kontak</h2>
         <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
       </div>
     );
